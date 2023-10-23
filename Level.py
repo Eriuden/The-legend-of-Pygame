6a2bbf1412cb2114fe5ebf46ae37a90a36de9362
Eriuden <antoinecarrette@hotmail.fr>
2023-10-23 12:24:09 +0200

    level

--- Level.py
+++ Level.py
@@ -1,4 +1,5 @@
 import pygame
+from Settings import *
 
 class Level:
     def __init__(self):
@@ -9,6 +10,24 @@ class Level:
         #Sprites
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        self.create_map()
+
+    # le MASSIF array de la world map a faire plus tard en settings...
+    # enumerate est conseillé pour indexer un array
+    # en énumerant ainsi les lignes, on peut faire la même avec les colonnes,
+    # contenues justement dans les lignes comme dans un array
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                #ca semble trompeur, mais c'est logique
+                # x pour les colonnes car celle tout à gauche, puis la seconde à 
+                # gauche, etc
+                # et y car les lignes sont empilées de haut en bas
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col
+
 
     def run(self):
         #mise à jour du jeu
--- Player.py
+++ Player.py
@@ -2,8 +2,8 @@ import pygame
 from pygame.sprite import _Group
 from Settings import *
 
-class Tile(pygame.sprite.Sprite):
+class player(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.image.load("../graphics/test/rock.png").convert_alpha()
+        self.image = pygame.image.load("../graphics/test/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
