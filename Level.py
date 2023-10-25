a4263755156fb0766cfa7ef08fbcc76a5e72f971
Eriuden <antoinecarrette@hotmail.fr>
2023-10-25 21:15:09 +0200

    graphics 2

--- Level.py
+++ Level.py
@@ -5,6 +5,7 @@ from Settings import *
 from Tile import tile
 from Player import player
 from Debug import debug
+from Support import *
 
 class Level:
     def __init__(self):
@@ -23,14 +24,23 @@ class Level:
     # en énumerant ainsi les lignes, on peut faire la même avec les colonnes,
     # contenues justement dans les lignes comme dans un array
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                #ca semble trompeur, mais c'est logique
-                # x pour les colonnes car celle tout à gauche, puis la seconde à 
-                # gauche, etc
-                # et y car les lignes sont empilées de haut en bas
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
+
+        layouts = {
+            "boundary": import_csv_layout("../map/map_FloorBlcoks.csv")
+        }
+
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(WORLD_MAP):
+                for col_index, col in enumerate(row):
+                    #ca semble trompeur, mais c'est logique
+                    # x pour les colonnes car celle tout à gauche, puis la seconde à 
+                    # gauche, etc
+                    # et y car les lignes sont empilées de haut en bas
+                    x = col_index * TILESIZE
+                    y = row_index * TILESIZE
+
+                    if style == "boundary":
+                        tile((x,y), [self.visible_sprites, self.obstacles_sprites], "invisible")
                 if col == "x" :
                     tile((x,y), [self.visible_sprites, self.obstacles_sprites])
                 if col =="p" :
