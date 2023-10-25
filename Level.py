aded0b741e67d5bf321c78bba0a857524a497686
Eriuden <antoinecarrette@hotmail.fr>
2023-10-25 09:24:44 +0200

    camera 2

--- Level.py
+++ Level.py
@@ -42,18 +42,27 @@ class Level:
 
     def run(self):
         #mise à jour du jeu
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
 
 class ySortCameraGroup(pygame.sprite.Group):
     def __init__(self):
 
         super().__init__()
+        # gestion de la caméra au propre
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0]//2
+        self.half_height = self.display_surface.get_size()[1]//2
+        self.offset = pygame.math.Vector2()
 
     def custom_draw(self):
+        # La caméra suit l'avancée du perso
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
         for sprite in self.sprites():
-            self.display_surface.blit(sprite.image, sprite.rect)
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_position)
+    
+
 
         
