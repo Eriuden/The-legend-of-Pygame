d308214f73fffd452285fe5b8fe85a3cef8f09c7
Eriuden <antoinecarrette@hotmail.fr>
2023-10-27 22:21:49 +0200

    animate 3

--- Player.py
+++ Player.py
@@ -14,6 +14,8 @@ class player(pygame.sprite.Sprite):
 
         self.import_player_assets()
         self.status = "down"
+        self.frame_index = 0
+        self.animation_speed = 0.5
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
@@ -31,42 +33,43 @@ class player(pygame.sprite.Sprite):
 
         for animation in self.animations.keys():
             full_path = character_path + animation
-            self.animations[animation] = import_folder
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
-        keys = pygame.key.get_pressed()
+        if not self.attacking:
+            keys = pygame.key.get_pressed()
 
-        # L'on est obligé de faire en deux variantes if du au else
-        # celui ci sert à stopper le mouvement si on arrête d'actionner
-        # mais comme il arrête le if statement, on en fait un pour chaque dimension
+            # L'on est obligé de faire en deux variantes if du au else
+            # celui ci sert à stopper le mouvement si on arrête d'actionner
+            # mais comme il arrête le if statement, on en fait un pour chaque dimension
 
-        #mouvement basiques
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-            self.status="up"
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-            self.status="down"
-        else:
-            self.direction.y = 0
+            #mouvement basiques
+            if keys[pygame.K_UP]:
+                self.direction.y = -1
+                self.status="up"
+            elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+                self.status="down"
+            else:
+                self.direction.y = 0
 
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-            self.status="left"
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-            self.status="right"
-        else:
-            self.direction.x = 0
-        #attaques, à noter, pygame ne semble pas avoir pensé aux boutons de la souris...
-        if keys[pygame.K_SPACE] and not self.attacking:
-            self.attacking = True
-            # le get ticks de cooldown fonctionnant indéfiniment, il pourra recharger celui ci, censé fonctionner qu'une fois à la base
-            self.attack_time = pygame.time.get_ticks()
-        #sorts
-        if keys[pygame.K_LSHIFT]and not self.attacking:
-            self.attacking = True
-            self.attack_time = pygame.time.get_ticks()
+            if keys[pygame.K_LEFT]:
+                self.direction.x = -1
+                self.status="left"
+            elif keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+                self.status="right"
+            else:
+                self.direction.x = 0
+            #attaques, à noter, pygame ne semble pas avoir pensé aux boutons de la souris...
+            if keys[pygame.K_SPACE]:
+                self.attacking = True
+                # le get ticks de cooldown fonctionnant indéfiniment, il pourra recharger celui ci, censé fonctionner qu'une fois à la base
+                self.attack_time = pygame.time.get_ticks()
+            #sorts
+            if keys[pygame.K_LSHIFT]:
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
 
     def get_status(self):
         if self.direction.x == 0 and self.direction.y == 0:
@@ -124,9 +127,19 @@ class player(pygame.sprite.Sprite):
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
                 
+    def animate(self):
+        animation = self.animations[self.status]
+
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
 
     def update(self):
         self.input()
         self.cooldown()
         self.get_status()
+        self.animate()
         self.move(self.speed)
