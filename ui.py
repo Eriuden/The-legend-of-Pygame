2951c57ce350aeeff5a936c8eb32103bab9af2de
Eriuden <antoinecarrette@hotmail.fr>
2023-10-30 11:05:15 +0100

    ui 4

--- ui.py
+++ ui.py
@@ -23,8 +23,17 @@ class UI:
         pygame.draw.rect(self.display_surface, color, current_rect)
         pygame.drax.rect(self.display_surface, UI_BORDERCOLOR, bg_rect, 3)
 
+    def show_exp(self, exp):
+        text_surface = self.font.render(str(int(exp)), False, TEXT_COLOR)
+        x = self.display_surface.get_size()[0] - 20
+        y = self.display_surface.get_size()[1] - 20
+        text_rect = text_surface.get_rect(bottomright = (x,y))
+
+        self.display_surface.blit(text_surface, text_rect)
 
     def display(self,player):
         #pygame.draw.rect(self.display_surface, "black", self.health_bar_rect)
         self.show_bar(player.healt, player.stats["health"], self.health_bar_rect,HEALT_COLOR)
         self.show_bar(player.energy, player.stats["energy"], self.energy_bar_rect,ENERGY_COLOR)
+
+        self.show_exp(player.exp)
