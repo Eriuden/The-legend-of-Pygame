6205deafe4ea46eda1b9cdb289affa113979ab9b
Eriuden <antoinecarrette@hotmail.fr>
2023-10-30 10:42:39 +0100

    ui 2

--- Settings.py
+++ Settings.py
@@ -9,8 +9,17 @@ BAR_HEIGHT = 20
 HEALTH_BAR_WIDTH = 200
 ENERGY_BAR_WIDTH = 140
 ITEM_BOX_SIZE = 80
-UIFONT = "../graphics/font/joystix.ttf"
+UI_FONT = "../graphics/font/joystix.ttf"
 UI_FONT_SIZE = 18
+
+WATER_COLOR = "#71ddee"
+UI_BGCOLOR = "#222222"
+UI_BORDERCOLOR = "#111111"
+TEXT_COLOR = "#EEEEEE"
+
+HEALT_COLOR = "red"
+ENERGY_COLOR = "blue"
+UI_BORDERCOLOR_ACTIVE = "gold"
 
 weapon_data = {
     "sword" : {"cooldown": 100, "damage": 15, "graphic": "../graphics/weapons/sword/full.png"},
