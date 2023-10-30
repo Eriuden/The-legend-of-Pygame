1d4270bf150924d151973326af8e83a32d25e00f
Eriuden <antoinecarrette@hotmail.fr>
2023-10-30 10:57:32 +0100

    ui 3

--- ui.py
+++ ui.py
@@ -10,5 +10,21 @@ class UI:
         self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10, 30, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+    def show_bar(self,current, max, bg_rect, color):
+        pygame.draw.rect(self.display_surface, UI_BGCOLOR, bg_rect)
+
+        #Voilà donc comment on crée une jauge de vie qui s'adapte aux HP
+        # le ratio agira donc sur la largueur par un effet de pourcentage
+        ratio = current / max
+        current_width = bg_rect.width * ratio 
+        current_rect = bg_rect.copy()
+        current_rect_width = current_width
+
+        pygame.draw.rect(self.display_surface, color, current_rect)
+        pygame.drax.rect(self.display_surface, UI_BORDERCOLOR, bg_rect, 3)
+
+
     def display(self,player):
-        pass
+        #pygame.draw.rect(self.display_surface, "black", self.health_bar_rect)
+        self.show_bar(player.healt, player.stats["health"], self.health_bar_rect,HEALT_COLOR)
+        self.show_bar(player.energy, player.stats["energy"], self.energy_bar_rect,ENERGY_COLOR)
