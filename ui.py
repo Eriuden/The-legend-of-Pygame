86b410952e6c475ef405e391aaab693f8ed7b962
Eriuden <antoinecarrette@hotmail.fr>
2023-10-31 08:37:33 +0100

    ui 5

--- ui.py
+++ ui.py
@@ -10,8 +10,16 @@ class UI:
         self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10, 30, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+        #conversion de la liste des armes
+
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon["graphic"]
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
     def show_bar(self,current, max, bg_rect, color):
-        pygame.draw.rect(self.display_surface, UI_BGCOLOR, bg_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDERCOLOR, bg_rect,3)
 
         #Voilà donc comment on crée une jauge de vie qui s'adapte aux HP
         # le ratio agira donc sur la largueur par un effet de pourcentage
@@ -21,7 +29,7 @@ class UI:
         current_rect_width = current_width
 
         pygame.draw.rect(self.display_surface, color, current_rect)
-        pygame.drax.rect(self.display_surface, UI_BORDERCOLOR, bg_rect, 3)
+        pygame.draw.rect(self.display_surface, UI_BORDERCOLOR, bg_rect, 3)
 
     def show_exp(self, exp):
         text_surface = self.font.render(str(int(exp)), False, TEXT_COLOR)
@@ -31,9 +39,26 @@ class UI:
 
         self.display_surface.blit(text_surface, text_rect)
 
+    def selection_box(self,left,top):
+        bg_rect = pygame.Rect(left,top, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BGCOLOR, bg_rect)
+        pygame.drax.rect(self.display_surface, UI_BORDERCOLOR, bg_rect, 3)
+        return bg_rect
+
+    def weapon_overlay(self,weapon_index):
+        bg_rect = self.selection_box(10,630)
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(bg_rect.center)
+        self.display_surface.blit(weapon_surf,weapon_rect)
+
     def display(self,player):
         #pygame.draw.rect(self.display_surface, "black", self.health_bar_rect)
         self.show_bar(player.healt, player.stats["health"], self.health_bar_rect,HEALTH_COLOR)
         self.show_bar(player.energy, player.stats["energy"], self.energy_bar_rect,ENERGY_COLOR)
 
         self.show_exp(player.exp)
+
+        self.weapon_overlay(player.weapon_index)
+
+        self.selection_box(10,650) #l'arme
+        self.selection_box(80,635) #le sort
